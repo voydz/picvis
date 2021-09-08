ce186f4e812dd673fea0cee8004f1d2037ff8343
Felix Rudat <frudat@doegel.de>
2021-09-09 00:08:10 +0200

    updated image view

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,12 +4,14 @@ import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import firebase from '../../lib/firebaseApp'
+import { formatDate } from '../../lib/utils'
 import { Main } from '../../components/Layout'
 import { useUser } from '../../context/userContext'
 import { useImages } from './useImages'
@@ -26,17 +28,37 @@ const useStyles = makeStyles((theme) => ({
   },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
+    backgroundSize: 'cover',
+    '&:hover': {
+      backgroundSize: 'contain',
+    },
   },
 }))
+
+
+async function setDocument(hash, data) {
+  const db = firebase.firestore()
+
+  try {
+    await db.collection('images').doc(hash).update(data)
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 export default function Admin() {
   const classes = useStyles()
   const router = useRouter()
   const { loadingUser, user } = useUser()
   const { loadingImages, images } = useImages()
+
+  async function handleAccept({ hash }) {
+    await setDocument(hash, { approved: 1 })
+  }
+
+  async function handleDeny({ hash }) {
+    await setDocument(hash, { approved: 0 })
+  }
 
   useEffect(() => {
     if (!(user || loadingUser)) {
@@ -46,33 +68,41 @@ export default function Admin() {
   }, [router, loadingUser, user])
 
   return (
-    <Main dark title="Verwaltung">
+    <Main dark title="Bilderverwaltung">
       <Container className={classes.cardGrid} maxWidth="xl">
         {loadingImages ? (
           <CircularProgress />
         ) : (
           <Grid container spacing={4}>
-            {images.map((card, i) => (
-              <Grid item key={i} xs={12} sm={6} md={4} lg={3} xl={2}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    This is a media card. You can use this section to describe the content.
-                  </CardContent>
-                  <CardActions>
-                    <Button>
-                      Annehmen
-                    </Button>
-                    <Button>
-                      Löschen
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+            {images.length === 0 ? (
+                <Grid item>
+                  <Typography variant="h4">Keine weiteren Bilder.</Typography>
+                  <Typography variant="subtitle1">Neue Bilder erscheinen automatisch, wenn jemand welche geschickt hat.</Typography>
+                </Grid>
+            ) : (
+              images.map(image => (
+                <Grid item key={image.hash} xs={12} sm={6} md={4} lg={3} xl={2}>
+                  <Card className={classes.card}>
+                    <CardHeader
+                      avatar={' '}
+                      title={image.sender_id}
+                      subheader={formatDate(image.timestamp)}
+                    />
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={image.ext}
+                    />
+                    <CardActions>
+                      <Button onClick={() => handleAccept(image)}>
+                        Annehmen
+                      </Button>
+                      <Button onClick={() => handleDeny(image)}>
+                        Löschen
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
             ))}
           </Grid>
         )}
--- pages/admin/useImages.js
+++ pages/admin/useImages.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import firebase from '../../lib/firebaseApp'
 
-export function useImages() {
+export const IMAGES_APPROVED = 1
+export const IMAGES_DECLINED = 0
+export const IMAGES_PENDING = -1
+
+export function useImages(approved = IMAGES_PENDING) {
   const [images, setImages] = useState([])
   const [loadingImages, setLoadingImages] = useState(true) // Helpful, to update the UI accordingly.
 
   useEffect(() => {
     const db = firebase.firestore()
-    const q = db.collection('images').where('approved', '==', -1)
+    const q = db.collection('images').where('approved', '==', approved)
     const unsubscriber = q.onSnapshot((querySnapshot) => {
       try {
         const items = []
@@ -27,7 +31,7 @@ export function useImages() {
 
     // Unsubscribe listener on unmount
     return () => unsubscriber()
-  }, [])
+  }, [approved])
 
   return { loadingImages, images };
 }
